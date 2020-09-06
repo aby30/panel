87d18dc6fade855e5199cb093eeaedb12f863050
Abhipray <abhipray.shailendra3@gmail.com>
2020-09-06 16:26:45 +0530

    Updates

---
+++ precache-manifest.91e7e112cc73e579ce1aa95e605fea7a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6056c69009c2576d9dbd0e525a5c26f1",
+    "url": "/panel/index.html"
+  },
+  {
+    "revision": "c0497863c9fe6de51a7e",
+    "url": "/panel/static/css/main.1a8d6906.chunk.css"
+  },
+  {
+    "revision": "fde0e58d06f506fe9331",
+    "url": "/panel/static/js/2.5b3764d9.chunk.js"
+  },
+  {
+    "revision": "c0497863c9fe6de51a7e",
+    "url": "/panel/static/js/main.8dfd5e8c.chunk.js"
+  },
+  {
+    "revision": "481c99371e0df674de62",
+    "url": "/panel/static/js/runtime-main.760bfd64.js"
+  },
+  {
+    "revision": "8ed51a6aaa25388ada904a77c3457296",
+    "url": "/panel/static/media/fb.8ed51a6a.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/panel/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "798fb6f93a1d8962250302086ec91587",
+    "url": "/panel/static/media/moneyPlant.798fb6f9.svg"
+  },
+  {
+    "revision": "2ea85d7448475a744c1485c2eac3d3d1",
+    "url": "/panel/static/media/netflix-logo.2ea85d74.png"
+  },
+  {
+    "revision": "580b9bc9ce8f36172af36f7913cc53de",
+    "url": "/panel/static/media/notification.580b9bc9.svg"
+  }
+]);
